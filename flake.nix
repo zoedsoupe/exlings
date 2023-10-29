02f6d790fd00fd219004fdb85c11952240e3bf0b
Zoey de Souza Pessanha <zoey.spessanha@zeetech.io>
2023-10-29 18:21:11 -0300

    feat: update nixpkgs

--- flake.nix
+++ flake.nix
@@ -1,34 +1,41 @@
 {
-  outputs = { self, nixpkgs }:
-    let
-      system = "aarch64-darwin";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-      pkgs = import nixpkgs {
-        inherit system;
-      };
-    in
-    {
-      devShells."${system}".default = with pkgs; mkShell {
+  outputs = {
+    self,
+    nixpkgs,
+  }: let
+    system = "aarch64-darwin";
+
+    pkgs = import nixpkgs {
+      inherit system;
+    };
+
+    inherit (pkgs.beam) packagesWith interpreters;
+    erl = packagesWith interpreters.erlangR26;
+  in {
+    devShells."${system}".default = with pkgs;
+      mkShell {
         name = "exlings";
         packages = [
           zig
           xz
           _7zz
-          elixir_1_14
+          erl.elixir
         ];
       };
 
-      packages."${system}" = rec {
-        default = exlings;
+    packages."${system}" = rec {
+      default = exlings;
 
-        docker =
-          let
-            inherit (builtins) filterSource elem;
-            file_names = [ "mix.exs" "mix.lock" "lib" ];
-            files = filterSource (p: _: elem (baseNameOf p) file_names) ./.;
-            paths = with pkgs; [ stdenv git xz gnupg zig elixir_1_14 files ];
-          in
-          with pkgs; dockerTools.buildImage {
+      docker = let
+        inherit (builtins) filterSource elem;
+        file_names = ["mix.exs" "mix.lock" "lib"];
+        files = filterSource (p: _: elem (baseNameOf p) file_names) ./.;
+        paths = with pkgs; [stdenv git xz gnupg zig elixir_1_14 files];
+      in
+        with pkgs;
+          dockerTools.buildImage {
             name = "exlings";
             tag = "latest";
             copyToRoot = buildEnv {
@@ -36,8 +43,8 @@
               name = "exlings-root";
             };
             config = {
-              Entrypoint = [ "sh" ];
-              Env = [ "MIX_ENV=prod" ];
+              Entrypoint = ["sh"];
+              Env = ["MIX_ENV=prod"];
             };
             runAsRoot = ''
               mix do local.hex --force, local.rebar --force
@@ -46,29 +53,28 @@
             '';
           };
 
-        exlings =
-          let
-            inherit (pkgs) beam;
-            inherit (beam.interpreters) erlang;
+      exlings = let
+        inherit (pkgs) beam;
+        inherit (beam.interpreters) erlangR26;
 
-            beamPackages = beam.packagesWith erlang;
-            pname = "exlings";
-            version = "0.1.0";
-            src = ./.;
-          in
-          beamPackages.mixRelease {
-            inherit src pname version;
+        beamPackages = beam.packagesWith erlangR26;
+        pname = "exlings";
+        version = "0.1.0";
+        src = ./.;
+      in
+        beamPackages.mixRelease {
+          inherit src pname version;
 
-            elixir = pkgs.elixir_1_14;
-            nativeBuildInputs = with pkgs; [ zig xz _7zz ];
-            postBuild = "mix deps.loadpaths --no-deps-check";
-            mixFodDeps = beamPackages.fetchMixDeps {
-              inherit src version;
+          elixir = pkgs.elixir_1_15;
+          nativeBuildInputs = with pkgs; [zig xz _7zz];
+          postBuild = "mix deps.loadpaths --no-deps-check";
+          mixFodDeps = beamPackages.fetchMixDeps {
+            inherit src version;
 
-              pname = "mix-deps-${pname}";
-              sha256 = "hefyKmjYmf8s1JmzS2YM/WlXzC+FkfCEGRjh1a43OGo=";
-            };
+            pname = "mix-deps-${pname}";
+            sha256 = "hefyKmjYmf8s1JmzS2YM/WlXzC+FkfCEGRjh1a43OGo=";
           };
-      };
+        };
     };
+  };
 }
